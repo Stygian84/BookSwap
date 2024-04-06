8da3d685373ccd795994ecc60b16facfdb6f1510
Stygian84 <nicholasgandhi84@gmail.com>
2024-04-07 05:45:15 +0800

    Feature Added : Completed Chat

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import { database } from "../firebase";
 import { ref, onValue, off, push, query, orderByChild, limitToLast, serverTimestamp } from "firebase/database";
+import "../css/chat.css";
+import getUserData from "./getUserData";
 
 function Chat({ userId, secondUserId }) {
   const [messages, setMessages] = useState([]);
   const [messageText, setMessageText] = useState("");
-
+  const [showChat, setShowChat] = useState(false);
+  const [secondUserData, setSecondUserData] = useState();
+  
   useEffect(() => {
     let messagesRef;
     if (userId && secondUserId) {
@@ -30,6 +34,18 @@ function Chat({ userId, secondUserId }) {
     }
   }, [userId, secondUserId]);
 
+  // Convert secondUserUID to name
+  useEffect(() => {
+    const fetchSecondUserData = async () => {
+      if (secondUserId) {
+        const userData = await getUserData(secondUserId);
+        setSecondUserData(userData);
+      }
+    };
+
+    fetchSecondUserData();
+  }, [secondUserId]);
+
   const sendMessage = () => {
     if (messageText.trim() !== "") {
       if (userId && secondUserId) {
@@ -44,18 +60,43 @@ function Chat({ userId, secondUserId }) {
       }
     }
   };
-
+  // If view own profile, hide the chat component
+  if (!userId || !secondUserId) {
+    return null;
+  }
+  console.log(secondUserData)
   return (
     <div>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index}>
-            {message.text} (Sent by: {message.userId === userId ? "You" : "Other user"})
-          </li>
-        ))}
-      </ul>
-      <input type="text" value={messageText} onChange={(e) => setMessageText(e.target.value)} />
-      <button onClick={sendMessage}>Send</button>
+      {/* Floating chat button */}
+      {!showChat && (
+        <div className="chat-float" onClick={() => setShowChat(true)}>
+          Chat
+        </div>
+      )}
+      {/* Chat window */}
+      {showChat && (
+        <div className="chat-window">
+          <div className="chat-header">
+            <div className="chat-close" onClick={() => setShowChat(false)}>
+              ×
+            </div>
+            Chat with {secondUserData.name}
+          </div>
+          <div className="chat-body">
+            <ul>
+              {messages.map((message, index) => (
+                <li key={index}>
+                  {message.text} (Sent by: {message.userId === userId ? "You" : "Other user"})
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="chat-footer">
+            <input type="text" value={messageText} onChange={(e) => setMessageText(e.target.value)} />
+            <button onClick={sendMessage}>Send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
